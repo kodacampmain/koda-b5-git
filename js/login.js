b0919164ca3e1eb5795a6d409127aacd1e1072b9
Fakhri <mightydhiyamatra@yahoo.co.id>
2025-11-12 16:29:54 +0700

    feat[login]: add feature for user to login with email and password

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const email = "mail@mail.com";
+const password = "12345";
+
+const inputEmail = "mail@mail.com";
+const inputPass = "root";
+
+if (inputEmail === email && inputPass === password) {
+  console.log("Login Berhasil");
+} else {
+  console.log("Email/Password salah");
+}
