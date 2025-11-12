2644c6712864a4d6bc4485ab34ee56a55da07a0a
Fakhri <mightydhiyamatra@yahoo.co.id>
2025-11-12 16:20:05 +0700

    feat[fetch-user]: adds feature to fetch user list from jsonplaceholder.typicode.com/users

---
+++ js/fetch-user.js
@@ -0,0 +1,13 @@
+export async function fetchUser() {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const result = await response.json();
+    console.log(result[0]);
+  } catch (error) {
+    console.log(error);
+  }
+}
